aabd6d8780de5bebf56a9824f449ac4b3e57ed82
marianekk578@gmail.com <Xdmarian.578>
2019-09-18 23:49:13 +0200

    user controller and page implemented

--- src/main/java/com/warmup/noteapp/user/UserController.java
+++ src/main/java/com/warmup/noteapp/user/UserController.java
@@ -2,6 +2,7 @@ package com.warmup.noteapp.user;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -13,10 +14,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/user")
+    public String showUserPage(){
+        return "user";
+    }
+
     @GetMapping("/user/{id}")
     public Optional<User> showUserByID(@PathVariable long id){
             return userService.findUserByID(id);
         }
+
+    @GetMapping("/user/all")
+    public List<User> showAllUsers(){
+        return userService.showAllUsers();
+    }
 
     @PostMapping("/user")
     public void addUser(@RequestBody User user){
